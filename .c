f6aa3dd9da2908945b6a51e51e529c1e5e045d77
Rafael Silva Coronel <75585382+rafacoronel@users.noreply.github.com>
2020-12-06 22:31:49 +0000

    Update .c

--- .c
+++ .c
@@ -1,3 +1,72 @@
+struct Funcionarios{
+   char nome[15];
+   int num_telefone, num_filho, codigo;
+   char data_saida[15];
+};
+struct Funcionarios V[2];
+
+int inserirFuncionarios(struct Funcionarios *V){
+    int i;
+    
+    for(i=0;i<2;i++){
+        printf("Intro.o seu nome:");
+        fgets(V[i].nome, 15, stdin);
+        puts("------------------------");
+        setbuf(stdin, NULL);
+        printf("Intro.o numero telefone:");
+        scanf("%d\n", &V[i].num_telefone);
+        puts("------------------------");
+        setbuf(stdin, NULL);
+        printf("\nQtos filhos tem:");
+        scanf("%d\n", &V[i].num_filho);
+        puts("------------------------");
+        setbuf(stdin, NULL);
+        printf("Intro.o seu codigo:");
+        scanf("%d\n", &V[i].codigo);
+        puts("------------------------");
+        setbuf(stdin, NULL);
+        printf("Intro.a data que saiu da empresa:");
+        fgets(V[i].data_saida, 15, stdin);
+        puts("------------------------");
+        setbuf(stdin, NULL);
+        puts("\n\n");
+        return i;
+    }
+    return -1;
+}
+
+int buscaCodigo(struct Funcionarios *V, int N, int elem){
+    int i;
+    for(i=0;i<N;i++){
+        if(elem==V[i].codigo){
+            return i;
+        }
+    }
+    return -1;
+}
+int buscaNome(struct Funcionarios *V, int N, char *elem){
+    int i;
+    
+    for(i=0;i<N;i++){
+        if(strcmp(elem, V[i].nome)==0){
+            return i;
+        }
+    }
+    return -1;
+}
+int dadosFuncionarios(struct Funcionarios *V){
+    int i;
+    
+    for(i=0;i<2;i++){
+        printf("Nome do funcionario:\t%s\n", V[i].nome);
+        printf("Numero de telefone do funcionario:\t%d\n", V[i].num_telefone);
+        printf("Numero de filhos que possui:\t%d\n", V[i].num_filho);
+        printf("O seu codigo:\t%d\n", V[i].codigo);
+        printf("A data em que deixou a empresa:\t%s\n\n", V[i].data_saida);
+    }
+    return i;
+}
+
 int menu(int opcao) {
     printf("1-\n"); //opção 1
     printf("2-\n"); //opção 2
@@ -40,6 +109,9 @@ int main(int argc, char** argv) {
     char titulo[30];
 
     menu(escolha);
+    
+    inserirFuncionarios(&V);
+    dadosFuncionarios(&V);
 
     dadosFunci(chave, titulo, telefone);
     return (EXIT_SUCCESS);
